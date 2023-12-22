6033474f9c9841ff714b92b40e5a8bbd23ec6e20
kuo <charliech17@yahoo.com.tw>
2023-12-22 23:53:52 +0800

    add connect logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'connect_django';
+  apiGetAllStds(): void {
+    fetch("/api/get_all_student/")
+      .then(data => data.json())
+      .then(res => console.log(res))
+  }
+  apiAddStd(){
+    const data = {
+      "name": "Jack",
+      "gender": "man",
+      "age": 20,
+      "school": "師大",
+      "hobby": "玩遊戲"
+    }
+    fetch(
+      "/api/get_all_student/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data),
+      }
+      ).then()
+  }
 }
